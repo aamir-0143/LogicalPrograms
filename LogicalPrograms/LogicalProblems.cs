0ea0adc01d7a6cad1c4b0f3e2502c5c5b37b242f
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-02 01:05:27 +0530

    [Aamir6] Add ; Added FibonacciSeries Methods

---
+++ LogicalPrograms/LogicalProblems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class LogicalProblems 
+    {
+        public void FibonacciSeries()
+        {
+            int first = 0, second = 1, third = 0;
+            Console.WriteLine("Enter the number of series :");
+            int series = Convert.ToInt32(Console.ReadLine());
+            if (series <= 1)
+            {
+                Console.WriteLine(first);
+                return;
+            }
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for (int i = 2; i < series; i++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+                Console.WriteLine(third);
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicalPrograms
+{
+   class Programs  
+   {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        LogicalProblems series = new LogicalProblems();
+                        series.FibonacciSeries();
+                        break;
+ 
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
