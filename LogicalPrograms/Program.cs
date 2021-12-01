a71dc0a0506e432389e7352dbd7d575e685df8f7
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-02 01:25:53 +0530

    [Aamir6] Add ; Added PefectNumber

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,7 +7,8 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series");
+                Console.WriteLine(" 2.PerfectNumber \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -15,8 +16,14 @@
                         LogicalProblems series = new LogicalProblems();
                         series.FibonacciSeries();
                         break;
- 
+
                     case 2:
+                        LogicalProblems num = new LogicalProblems();
+                        num.PerfectNumber();
+                        break;
+
+
+                    case 3:
                         flag = false;
                         break;
                 }
